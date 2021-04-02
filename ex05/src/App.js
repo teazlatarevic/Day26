dca7a4263aa52196f8a5a5b6d4f8601ad250a564
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-04-02 14:03:28 +0200

    Solution for the example 5

---
+++ ex05/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostRequest from './PostRequest';
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequest />
+    </div>
+  );
+}
+
+export default App;
