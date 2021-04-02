0c16f8fd859c0a52eb104f4b727c3c495be3b9da
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-04-02 13:04:27 +0200

    Solution for the example 1

---
+++ ex01/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequest from './GetRequest';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequest />
+    </div>
+  );
+}
+
+export default App;
