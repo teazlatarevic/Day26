dfdac1ba630e291d41faca8396b71666f52bdd99
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-04-02 13:15:54 +0200

    Solution for the example 2

---
+++ ex02/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequestAsyncAwait from './GetRequestAsyncAwait';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestAsyncAwait />
+    </div>
+  );
+}
+
+export default App;
