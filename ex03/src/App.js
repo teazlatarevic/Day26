be23941214fdaf55c47afc17fe1e948c52092b0e
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-04-02 13:27:48 +0200

    Solution for the example 3

---
+++ ex03/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequestErrorHandling from './GetRequestErrorHandling';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestErrorHandling />
+    </div>
+  );
+}
+
+export default App;
