c36a5125739d886bed93213ba77ea1427000d1a3
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-04-02 14:14:47 +0200

    Solution for the example 6

---
+++ ex06/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostRequestAsyncAwait from './PostRequestAsyncAwait';
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestAsyncAwait />
+    </div>
+  );
+}
+
+export default App;
