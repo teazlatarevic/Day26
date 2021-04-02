4ce388ccff0ada73e4efd9f98556a6e3e548e2a3
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-04-02 21:40:13 +0200

    Solution for the example 7

---
+++ ex07/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostRequestErrorHandling from './PostRequestErrorHandling';
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestErrorHandling />
+    </div>
+  );
+}
+
+export default App;
