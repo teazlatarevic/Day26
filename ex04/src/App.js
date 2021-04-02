536acc7fa75eb31386f150b17f351f033c06c396
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-04-02 13:50:45 +0200

    Solution for the example 4

---
+++ ex04/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequestSetHeaders from './GetRequestSetHeaders';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestSetHeaders />
+    </div>
+  );
+}
+
+export default App;
